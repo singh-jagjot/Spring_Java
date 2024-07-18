3cd160a3beb19d89f3404249c8b37a897efcd782
singh-jagjot <singh.jagjot7@gmail.com>
2024-07-18 06:08:55 -0400

    +debug logs

--- Speer_Project/src/main/java/com/backend/project/config/TokenFilter.java
+++ Speer_Project/src/main/java/com/backend/project/config/TokenFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,6 +17,7 @@ import java.io.IOException;
 @Configuration
 public class TokenFilter extends OncePerRequestFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(TokenFilter.class);
     private final JwtService util;
 
     @Autowired
@@ -24,6 +27,7 @@ public class TokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("Someone hit '{}'", request.getRequestURL());
         if (request.getRequestURI().startsWith("/api/auth/")
                 || request.getRequestURI().startsWith("/v3/api-docs")
                 || request.getRequestURI().startsWith("/swagger-ui")
